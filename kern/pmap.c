9057a3308812869c70951b156389a3c2b9013d1d
Tarik <tarik.hamidovic@fet.ba>
2019-12-10 18:32:38 +0000

    page_free() implementacija

--- kern/pmap.c
+++ kern/pmap.c
@@ -328,6 +328,10 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
+  if (pp ->pp_ref != 0 || pp ->pp_link != NULL) 
+    panic("Invalid page free!");
+  pp ->pp_link = page_free_list;
+  page_free_list = pp;
 }
 
 //
