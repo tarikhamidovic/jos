c95608703ce9f268e4d77b5de92237f190d97712
Tarik <tarik.hamidovic@fet.ba>
2020-02-07 17:32:34 +0000

    exercise 6 zavrsen

--- kern/sched.c
+++ kern/sched.c
@@ -29,7 +29,17 @@ sched_yield(void)
 	// below to halt the cpu.
 
 	// LAB 4: Your code here.
-
+  
+  size_t i, index = curenv ? ENVX(curenv->env_id) : 0;
+  for (i = index; i != index + NENV; ++i) {
+    if (envs[i%NENV].env_status == ENV_RUNNABLE) {
+      env_run(envs + (i%NENV));
+      break;
+    }
+  }
+  if (curenv && curenv->env_status == ENV_RUNNING) {
+    env_run(curenv);
+  }
 	// sched_halt never returns
 	sched_halt();
 }
