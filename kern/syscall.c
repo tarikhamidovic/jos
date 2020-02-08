c15d16badca18faed477c6fa6ca0e3cfe092c93f
Tarik <tarik.hamidovic@fet.ba>
2020-02-08 20:22:29 +0000

    exercise 10 i 11 zavrseni

--- kern/syscall.c
+++ kern/syscall.c
@@ -141,7 +141,7 @@ sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
 	struct Env *e;
-  if (envid2env(envid, &e, 1) < 0) 
+  if (envid2env(envid, &e, 1) < 0)
     return -E_BAD_ENV;
   e->env_pgfault_upcall = func;
   return 0;
@@ -373,6 +373,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
       return sys_page_map(a1, (void *) a2, a3, (void *) a4, a5);
     case SYS_page_unmap:
       return sys_page_unmap(a1, (void *) a2);
+    case SYS_env_set_pgfault_upcall:
+      return sys_env_set_pgfault_upcall(a1, (void *) a2);
 
     default:
 		  return -E_INVAL;
